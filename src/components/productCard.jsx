7ec6b533224bb49cf818b0414dda6e4fc5809e73
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-09-05 16:43:12 +0300

    updating the html

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,23 +1,38 @@
-import'./style.css';
-import Desktop from '../assets/images/image-product-desktop.jpg';
-import Mobile from '../assets/images/image-product-mobile.jpg';
-
+import "./style.css";
+import Desktop from "../assets/images/image-product-desktop.jpg";
+import Mobile from "../assets/images/image-product-mobile.jpg";
+import Cart from "../assets/images/icon-cart.svg"
 
 const productCard = () => {
   return (
     <main>
-         <div className='product_card'>
-         <div className='product_card_right'>
-            <picture>
-                <source srcSet={Desktop} media="(min-width:48rem)"/>
-                <img src={Mobile} alt="Perfume product"/>
-            </picture> 
-         </div>
-         <div className='product_card_left'></div>
+      <div className="product_card">
+        <div className="product_card_right">
+          <picture>
+            <source srcSet={Desktop} media="(min-width:48rem)"/>
+            <img src={Mobile} alt="Appearance of perfume product " />
+          </picture>
+        </div>
 
-         </div>
+        <div className="product_card_left">
+          <p className="product_text">Perfume</p>
+          <h2 className="product_heading"> Gabrielle Essence Eau De Parfum</h2>
+          <p className="product_description">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <span className="product_price">$149.99</span>
+          <s> $169.99</s>
+           <div className="product_btn">
+           <button type="button">
+            <img src={Cart} alt="adding to cart"/>
+            <span>Add to cart</span>
+          </button>
+           </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default productCard
+export default productCard;
